24624a5c9e683e6de33b570c902db5443aa4a423
Koh Meng Wang <mengwangk@gmail.com>
2019-02-02 22:32:09 +0800

    Initial commit

---
+++ .bash/path.sh
@@ -0,0 +1,8 @@
+# set common path
+export WORKSPACE=$HOME/workspace
+export DEV=$WORKSPCE/dev
+
+# set GO path
+export GOPATH=$DEV/go-lang/getting-started
+export GOBIN=$DEV/go-lang/getting-started/bin
+
---
+++ .bashrc
@@ -0,0 +1,7 @@
+if [ -f ~/.bash/variables.sh ]; then
+    source ~/.bash/variables.sh
+fi
+
+if [ -f ~/.bash/prompt.sh ]; then
+    source ~/.bash/prompt.sh
+fi
