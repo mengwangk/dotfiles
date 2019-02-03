edac4000dabf6247c444e0095e61559882b7fdaf
Koh Meng Wang <mengwangk@gmail.com>
2019-02-03 22:55:56 +0800

    Updated

--- .bash/path.sh
+++ .bash/path.sh
@@ -3,6 +3,9 @@ export WORKSPACE=$HOME/workspace
 export DEV=$WORKSPACE/dev
 
 # set GO path
-export GOPATH=$DEV/go-lang/getting-started
-export GOBIN=$DEV/go-lang/getting-started/bin
+export GOPATH=$DEV/go-lang
+export GOBIN=$DEV/go-lang/bin
+
+# set PATH
+export PATH=$PATH:$(go env GOPATH)/bin
 
