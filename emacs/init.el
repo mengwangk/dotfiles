c8fcf183948c4c9b77b1b62e62683b186f391227
Koh Meng Wang <mengwangk@gmail.com>
2020-09-23 11:53:39 +0800

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil org ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
+; (package-initialize)
+; (package-refresh-contents)
 
-;; Download Evil
+;; evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
