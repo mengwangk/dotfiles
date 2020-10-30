bd2f8f7f57449deed7258928f45bdaa0b68e66e2
Koh Meng Wang <mengwangk@gmail.com>
2020-10-30 22:50:12 +0800

    Update emacs config

--- doom/packages.el
+++ doom/packages.el
@@ -8,6 +8,7 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 (package! command-log-mode)
+(package! org-noter)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
