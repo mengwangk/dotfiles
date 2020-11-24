e1f15cada986265877ca75a8ea80e035616d1d2c
Meng Wang <mengwangk@gmail.com>
2020-11-24 21:43:54 +0800

    Update config

--- config/emacs/elpa/command-log-mode-20160413.447/command-log-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/mengwangk/.emacs.d/elpa/command-log-mode-20160413.447/command-log-mode.el  -*- no-byte-compile: t -*-
-(define-package "command-log-mode" "20160413.447" "log keyboard commands to buffer" 'nil :commit "af600e6b4129c8115f464af576505ea8e789db27" :keywords '("help") :authors '(("Michael Weber" . "michaelw@foldr.org")) :maintainer '("Michael Weber" . "michaelw@foldr.org") :url "https://github.com/lewang/command-log-mode")
