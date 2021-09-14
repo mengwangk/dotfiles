92f75221f7b34bc55aae2511d382929f1146f71e
alpha2phi <alpha2phi@gmail.com>
2021-09-14 22:14:50 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ pip_install() {
 }
 
 npm_install() {
-  sudo npm i -g pyright typescript  typescript-language-server vim-language-server nodemon yarn write-good
+  sudo npm i -g pyright typescript  typescript-language-server vim-language-server nodemon yarn write-good prettier eslint
 }
 
 jump_install(){
