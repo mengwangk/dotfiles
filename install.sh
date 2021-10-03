a25ff72aae6bb300c047ec3ba2dc018ad06c146e
alpha2phi <alpha2phi@gmail.com>
2021-10-03 15:40:36 +0800

    Update

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ p10k_install() {
 }
 
 yay_install() {
-  yay -S fakeroot tree-sitter stow binutils neovim-nightly-bin ttf-fira-code nerd-fonts-fira-code gcc make nodejs npm brave-bin google-chrome wezterm shfmt peek screenkey ripgrep fd unzip cmake flameshot neofetch ninja tldr tealdeer pet sway broot mcfly git-delta diff-so-fancy lldb visual-studio-code-bin aria2 watchman-bin xclip xsel lazygit
+  yay -S fakeroot tree-sitter stow binutils neovim-nightly-bin ttf-fira-code nerd-fonts-fira-code gcc make nodejs npm brave-bin google-chrome wezterm shfmt peek screenkey ripgrep fd unzip cmake flameshot neofetch ninja tldr tealdeer pet sway broot mcfly git-delta diff-so-fancy lldb visual-studio-code-bin aria2 watchman-bin xclip xsel lazygit ueberzug
 }
 
 pip_install() {
   # Install miniconda first
-  pip install pynvim pyfunky csvkit jupyter jupyterlab setuptools pyre_check wheel fb-sapp black flake8 isort
+  pip install pynvim pyfunky csvkit jupyter jupyterlab setuptools pyre_check wheel fb-sapp black flake8 isort ranger-fm
 }
 
 npm_install() {
